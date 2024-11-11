cb354be280a72b5438a60d8006ddedd8519fff68
tewodros-mekonnen <you@example.com>
2024-11-11 14:05:32 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Welcome!';
+  message = 'This is my Todo list';
 }
